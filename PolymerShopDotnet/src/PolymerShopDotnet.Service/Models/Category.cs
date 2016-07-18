d5043d601270e4c13a42d4852744c6abef81b687
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-18 23:24:09 +0200

    Scaffold Web API endpoint for Categories

---
+++ PolymerShopDotnet/src/PolymerShopDotnet.Service/Models/Category.cs
@@ -0,0 +1,10 @@
+namespace PolymerShopDotnet.Service.Models
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Placeholder { get; set; }
+    }
+}
