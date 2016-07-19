42145a9ea60a51e7037e66ba32b12bfbc2078ac0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-07-19 23:22:59 +0200

    Update Categories controller
    - metadata for Swagger UI
    - example data mockups

--- PolymerShopDotnet/src/PolymerShopDotnet.Service/Controllers/CategoriesController.cs
+++ PolymerShopDotnet/src/PolymerShopDotnet.Service/Controllers/CategoriesController.cs
@@ -3,44 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PolymerShopDotnet.Service.Models;
+using Swashbuckle.SwaggerGen.Annotations;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PolymerShopDotnet.Service.Controllers
 {
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class CategoriesController : Controller
     {
         // GET api/values
+        /// <summary>
+        /// Returns list of categories
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [ProducesResponseType(typeof(IEnumerable<Category>), 200)]
+        [SwaggerResponse(System.Net.HttpStatusCode.OK, Type = typeof(int))]
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            List<Category> categories = new List<Category>();
+            return Ok(categories);
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/categories/mens_outerwear
+        /// <summary>
+        /// Returns category by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("{name}")]
+        [ProducesResponseType(typeof(Category), 200)]
+        [SwaggerResponse(System.Net.HttpStatusCode.OK, Type = typeof(int))]
+        public IActionResult Get(string name)
         {
-            return "value";
+            Category category = new Category()
+            {
+                Name = "mens_outerwear",
+                Title = "Men\"s Outerwear",
+                Image = "/images/mens_outerwear.jpg",
+                Placeholder = "data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXAABAQEAAAAAAAAAAAAAAAAAAAIEAQEAAAAAAAAAAAAAAAAAAAACEAAAAwYHAQAAAAAAAAAAAAAAERMBAhIyYhQhkaEDIwUVNREBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A3dkr5e8tfpwuneJITOzIcmQpit037Bw4mnCVNOpAAQv/2Q=="
+            };
+            return Ok(category);
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
